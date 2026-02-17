7faf4066f04cc26278e5b999ba6745c6d1a9b16f
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-02-17 22:02:26 +0200

    ntdll: Call virtual_init from JNI_OnLoad to avoid early virtual_alloc_first_teb failure.
    Wine frequently failed early during startup with:
    
    ```
    err:virtual:virtual_alloc_first_teb wine: failed to map the shared user data: c0000018
    ```
    
    This appears to happen because the JVM can reserve address space that
    Wine later needs at a fixed address for the shared user data
    mapping. Initializing the virtual memory subsystem earlier reduces the
    chance of the JVM taking that range.
    
    Signed-off-by: Twaik Yont <9674930+twaik@users.noreply.github.com>
    (cherry picked from commit 740961fef061dfa92f61014ee342b91aabc8bf7e)

--- dlls/ntdll/unix/loader.c
+++ dlls/ntdll/unix/loader.c
@@ -2601,7 +2601,6 @@ static jstring wine_init_jni( JNIEnv *env, jobject obj, jobjectArray cmdline, jo
     main_argv = argv;
 
     init_paths();
-    virtual_init();
     init_environment();
 
 #ifdef __i386__
@@ -2628,6 +2627,8 @@ jint JNI_OnLoad( JavaVM *vm, void *reserved )
 
     JNIEnv *env;
     jclass class;
+
+    virtual_init();
 
     java_vm = vm;
     if ((*vm)->AttachCurrentThread( vm, &env, NULL ) != JNI_OK) return JNI_ERR;
