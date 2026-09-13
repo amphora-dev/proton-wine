360089be262c4a073284c5353a2c78f05e1fc0a7
skywalker512 <houzhenhong@outlook.com>
2026-09-13 18:33:00 +0000

    wineandroid: null-check p_java_vm in Amphora host mode.

--- dlls/wineandroid.drv/init.c
+++ dlls/wineandroid.drv/init.c
@@ -86,7 +86,7 @@ void init_monitors( int width, int height )
            wine_dbgstr_rect( &rect ), wine_dbgstr_rect( &monitor_rc_work ));
 
     /* if we're notified from Java thread, update registry */
-    if (*p_java_vm) NtUserCallNoParam( NtUserCallNoParam_DisplayModeChanged );
+    if (p_java_vm && *p_java_vm) NtUserCallNoParam( NtUserCallNoParam_DisplayModeChanged );
 }
 
 
@@ -170,7 +170,7 @@ void set_screen_dpi( DWORD dpi )
  */
 static void fetch_display_metrics(void)
 {
-    if (*p_java_vm) return;  /* for Java threads it will be set when the top view is created */
+    if (p_java_vm && *p_java_vm) return;  /* for Java threads it will be set when the top view is created */
 
     SERVER_START_REQ( get_window_rectangles )
     {
